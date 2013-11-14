a7ebeabbc05dc4a80143f11b7be76d2841c01ac6
Filipe Alberto <albertof@uwm.edu>
2013-11-14 00:00:00 +0000

    version 1.05

--- R/read.frag.sizes.R
+++ R/read.frag.sizes.R
@@ -1,36 +1,49 @@
+#reads fragment data from other formats than .ah (STrand)
+ read.frag.sizes<-function (in.file, date, plate) {
+   
+ GMdata <- read.table(file = in.file, sep = "\t", header = TRUE)
+
+nr<-nrow(GMdata)
+TAB<-NULL
 
 
-read.frag.sizes<-function (in.file, date, plate) 
-{
-    GMdata <- read.table(file = in.file, sep = "\t", header = TRUE)
-    r <- 1
+
+for(r in 1:nr){
+
     if (is.na(GMdata[r, 4])) {
         r <- r + 1
     }
     else {
         if (GMdata[r, 4] == GMdata[r, 5]) {
-            TAB <- data.frame(Marker = GMdata[r, 3], Sample = GMdata[r, 1], Fragment = GMdata[r, 4], Date = date, Plate = plate)
+            Tab <- data.frame(Marker = GMdata[r, 3], 
+                              Sample = GMdata[r,1],
+                              Fragment = GMdata[r, 4],
+                              Date = date,
+                              Plate = plate)
         }
         else {
-            TAB <- rbind(data.frame(Marker = GMdata[r, 3], Sample = GMdata[r, 1], Fragment = GMdata[r, 4], Date = date, Plate = plate),  data.frame(Marker = GMdata[r, 3], Sample = GMdata[r, 1], Fragment = GMdata[r, 5], Date = date, Plate = plate))
-        }
-    }
-    r <- r + 1
-    repeat {
-        if (!is.na(GMdata[r, 4])) {
-            if (GMdata[r, 4] == GMdata[r, 5]) {
-                Tab <- data.frame(Marker = GMdata[r, 3], Sample = GMdata[r,1], Fragment = GMdata[r, 4], Date = date, Plate = plate)
-                TAB <- rbind(TAB, Tab)
-            }
-            else {
-                Tab <- rbind(data.frame(Marker = GMdata[r, 3],  Sample = GMdata[r, 1], Fragment = GMdata[r, 4], Date = date, Plate = plate), data.frame(Marker = GMdata[r,3], Sample = GMdata[r, 1], Fragment = GMdata[r, 5], Date = date, Plate = plate))
-                TAB <- rbind(TAB, Tab)
-            }
+            Tab <- rbind(data.frame(Marker = GMdata[r, 3],
+                                     Sample = GMdata[r,1], 
+                                     Fragment = GMdata[r, 4], 
+                                     Date = date,
+                                     Plate = plate), 
+                         data.frame(Marker = GMdata[r, 3],
+                                    Sample = GMdata[r,1],
+                                    Fragment = GMdata[r, 5], 
+                                    Date = date,
+                                    Plate = plate)
+                         )
         }
         r <- r + 1
-        if (r > length(GMdata[, 1])) 
-            break
     }
-    TAB
+if(exists("Tab")){  TAB <- rbind(TAB, Tab)
+		     rm(Tab)
+                  }
+
+  }
+  TAB
 }
+
+
+
 

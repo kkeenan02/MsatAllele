81d5cda63c2e630903537cfc971e6e3aed30b0d3
kkeenan02 <kkeenan02@qub.ac.uk>
2014-09-15 16:04:56 +0100

    fixing factor issues

--- R/fastReadFrag.R
+++ R/fastReadFrag.R
@@ -3,8 +3,7 @@
 #' Kevin Keenan, 2014
 
 fastReadFrag <- function(in.file, date, plate){
-  GMdata <- read.table(file = in.file, sep = "\t", header = TRUE,
-                       stringsAsFactors = FALSE)
+  GMdata <- read.table(file = in.file, sep = "\t", header = TRUE)
   TAB <- apply(GMdata, 1, function(x){
     if(is.na(x[4])){
       return(c(NA, NA, NA, NA, NA))
@@ -17,9 +16,10 @@ fastReadFrag <- function(in.file, date, plate){
   })
   TAB <- do.call("rbind", TAB)
   MS <- apply(TAB, 1, function(x) any(is.na(x)))
-  TAB <- TAB[!MS, ]
   colnames(TAB) <- c("Marker", "Sample", "Fragment", "Date", "Plate")
   TAB <- as.data.frame(TAB)
-  TAB$Fragment <- as.numeric(TAB$Fragment)
+  TAB$Sample <- factor(TAB$Sample, levels(GMdata$Sample.Name))
+  TAB$Fragment <- as.numeric(levels(TAB$Fragment))[TAB$Fragment]
+  TAB <- TAB[!MS, ]
   return(TAB)
 }

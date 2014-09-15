4dee9a7ed25a8a2b66dde9ea256da302c2edf2dc
kkeenan02 <kkeenan02@qub.ac.uk>
2014-09-15 14:24:12 +0100

    updates

---
+++ R/fastReadFrag.R
@@ -0,0 +1,24 @@
+#' A rewrite of the read.frag.sizes function from the MsatAllele package
+#' 
+#' Kevin Keenan, 2014
+
+fastReadFrag <- function(in.file, date, plate){
+  GMdata <- read.table(file = in.file, sep = "\t", header = TRUE,
+                       stringsAsFactors = FALSE)
+  TAB <- apply(GMdata, 1, function(x){
+    if(is.na(x[4])){
+      return(c(NA, NA, NA, NA, NA))
+    } else if(x[4] == x[5]){
+      return(c(x[c(3,1,4)], date, plate))
+    } else {
+      return(rbind(c(x[c(3,1,4)], date, plate),
+                   c(x[c(3,1,5)], date, plate)))
+    }
+  })
+  TAB <- do.call("rbind", TAB)
+  MS <- apply(TAB, 1, function(x) any(is.na(x)))
+  TAB <- TAB[!MS, ]
+  colnames(TAB) <- c("Marker", "Sample", "Fragment", "Date", "Plate")
+  TAB <- as.data.frame(TAB)
+  return(TAB)
+}

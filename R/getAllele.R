c4aeadfa3ac7f8a020b047c401bc4d115bef1a3d
kkeenan02 <kkeenan02@qub.ac.uk>
2014-09-16 12:20:13 +0100

    test

--- R/getAllele.R
+++ R/getAllele.R
@@ -13,7 +13,7 @@ getAllele <- function(LocusDBF, Marker, fragment){
   if(j < Lcheck - j){
     Allrange <- Checkrange[(j+1):Lcheck]
   } else {
-    Allrange <- Checkrange[1:j]) 
+    Allrange <- Checkrange[1:j] 
   }
   Allele.code <- round(median(Allrange), digits = 0)
   ifelse(is.na(Allele.code), round(fragment, digits = 0), Allele.code)

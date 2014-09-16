20a7e5383bfb5c33a9d849f8dae83c89da31cf87
kkeenan02 <kkeenan02@qub.ac.uk>
2014-09-16 13:15:47 +0100

    test

--- R/getAllele.R
+++ R/getAllele.R
@@ -1,15 +1,15 @@
 getAllele <- function(LocusDBF, Marker, fragment){
   #LocusDBF <- MsatAllele::OrderByLocus(RefDB,Marker)
   c1<-(LocusDBF[,2]>=fragment-0.8 & LocusDBF[,2]<=fragment+0.8)
-  Checkrange<-LocusDBF[c1,2]
+  Checkrange <- LocusDBF[c1,2]
   j<-1
   dif<-0
   Lcheck<-length(Checkrange)
-  while(j < Lcheck){
-    dif<-Checkrange[j+1]-Checkrange[j]
-    if(dif>=0.4)break
-    j<-j+1 
-  }
+#   while(j < Lcheck){
+#     dif <- checkrange[j+1] - checkrange[j]
+#     if(dif>=0.4)break
+#     j<-j+1 
+#   }
   if(j < Lcheck - j){
     Allrange <- Checkrange[(j+1):Lcheck]
   } else {

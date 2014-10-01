fd6217431fc74e727939a5ea81a9031da1cee334
kkeenan02 <kkeenan02@qub.ac.uk>
2014-10-01 18:17:14 +0100

    add long form db functionality

--- R/fastReadFrag.R
+++ R/fastReadFrag.R
@@ -2,18 +2,29 @@
 #' 
 #' Kevin Keenan, 2014
 
-fastReadFrag <- function(in.file, date, plate){
+fastReadFrag <- function(in.file, date, plate, long = FALSE){
   GMdata <- read.table(file = in.file, sep = "\t", header = TRUE)
-  TAB <- apply(GMdata, 1, function(x){
-    if(is.na(x[4])){
-      return(c(NA, NA, NA, NA, NA))
-    } else if(x[4] == x[5]){
-      return(c(x[c(3,1,4)], date, plate))
-    } else {
-      return(rbind(c(x[c(3,1,4)], date, plate),
-                   c(x[c(3,1,5)], date, plate)))
-    }
-  })
+  if(!long){
+    TAB <- apply(GMdata, 1, function(x){
+      if(is.na(x[4])){
+        return(c(NA, NA, NA, NA, NA))
+      } else if(x[4] == x[5]){
+        return(c(x[c(3,1,4)], date, plate))
+      } else {
+        return(rbind(c(x[c(3,1,4)], date, plate),
+                     c(x[c(3,1,5)], date, plate)))
+      }
+    })
+  } else {
+    TAB <- apply(GMdata, 1, function(x){
+      if(is.na(x[4])){
+        return(c(NA, NA, NA, NA, NA))
+      } else {
+        return(rbind(c(x[c(3,1,4)], date, plate),
+                     c(x[c(3,1,5)], date, plate)))
+      }
+    })
+  }
   TAB <- do.call("rbind", TAB)
   MS <- apply(TAB, 1, function(x) any(is.na(x)))
   colnames(TAB) <- c("Marker", "Sample", "Fragment", "Date", "Plate")
